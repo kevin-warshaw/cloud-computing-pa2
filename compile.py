77797f545f25ad1d6bac85e0c99cb9890858229b
kevin-warshaw <31418628+kevin-warshaw@users.noreply.github.com>
2018-02-13 23:09:30 -0500

    Files needed for automatic grader

---
+++ compile.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python import os
+import subprocess
+  2
+subprocess.call("rm -f ./a.out", shell=True)
+retcode = subprocess.call("/usr/bin/g++ uploads/walk.cc", shell=True) if retcode:
+print("failed to compile walk.cc") exit
+subprocess.call("rm -f ./output", shell=True) retcode = subprocess.call("./test.sh", shell=True)
+print ("Score: " + str(retcode) + " out of 2 correct.")
+print("*************Original submission*************") with open('uploads/walk.cc','r') as fs:
+print(fs.read())
